0e7b9d4c2a9510e40e3fa43f09216d4037fbeb17
Ivan Solnyshkin <ethosy@yandex.ru>
2020-11-01 01:46:12 +0700

    feat: Инициированное приложение, убрано лишнее, добавлены шрифты Inter, набросан шаблон файловой структуры

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="page">
+      <h1 className="page__title">Тут будет проект</h1>
+    </div>
+  );
+}
+
+export default App;
