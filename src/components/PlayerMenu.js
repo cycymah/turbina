488c85a073db1f590b7e3d20b40a79fa47a8440d
Ivan Solnyshkin <ethosy@yandex.ru>
2020-11-03 20:04:11 +0700

    feat: Полностью решена с частым перерендера компонента плеера при проигрывании музыки, добавлен блок по плейлист/инфо

---
+++ src/components/PlayerMenu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PlayerMenu = () => {
+  return (
+    <ul>
+      <li>Песня 1</li>
+      <li>Песня 2</li>
+      <li> Песяня 3</li>
+    </ul>
+  );
+};
+
+export default PlayerMenu;
