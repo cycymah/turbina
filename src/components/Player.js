920eb37087df629bcd02871fdfb0abdfdfbdfeb6
Ivan Solnyshkin <ethosy@yandex.ru>
2020-11-03 11:48:53 +0700

    fix: небольшие правки header feat: переделан плеер. Теперь он отображает время и идет строка состояния

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,34 +1,44 @@
-/* eslint-disable react/prop-types */
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, createRef } from 'react';
 import './Player.css';
 import song from '../Float.mp3';
 
 const Player = () => {
   const [isSongPlay, setSongPlay] = useState(false);
-  const [songTime, setSongTime] = useState('');
+  const [songTime, setSongTime] = useState();
+  const [currentSongTime, setCurrentSongTime] = useState(0);
+  const [styleSeekerCover, setSeekerCover] = useState('0%');
+
+  let audioElement = createRef();
 
   useEffect(() => {
-    const audioFile = document.querySelector('.player__audio');
     if (isSongPlay) {
-      audioFile.play();
+      audioElement.play();
     } else {
-      audioFile.pause();
+      audioElement.pause();
     }
   }, [isSongPlay]);
-
   useEffect(() => {
-    // console.log(audioFile);
-    setSongTime(1);
-    // console.log(audioFile.duration);
-  }, [songTime]);
+    setCurrentSongTime(audioElement.currentTime);
+  });
+
+  const onTimeUpdateTimer = () => {
+    let songDuration =
+      Math.round(currentSongTime / 60) + ':' + Math.round(currentSongTime);
+    setSongTime(songDuration);
+    setSeekerCover((currentSongTime * 100) / audioElement.duration);
+  };
 
   const handlePlayCLick = () => {
+    setCurrentSongTime(audioElement.currentTime);
     isSongPlay ? setSongPlay(false) : setSongPlay(true);
   };
 
   return (
     <section className="player">
-      <audio className="player__audio">
+      <audio
+        className="player__audio"
+        ref={(audio) => (audioElement = audio)}
+        onTimeUpdate={onTimeUpdateTimer}>
         Your browser does not support the
         <code>audio</code> element.
         <source src={song} type="audio/mp3"></source>
@@ -39,10 +49,20 @@ const Player = () => {
         className={`player__play-btn ${
           isSongPlay ? 'player__play-btn_pause' : 'player__play-btn_play'
         }`}></button>
-      <p className="player__song-info">Песня sdfsdf</p>
-      <span className="player__song-time">2:30</span>
-      <input type="range" className="player__seeker" />
-      <div className="player__songs-list"></div>
+      <div className="player__container">
+        <div className="player__info-box">
+          <p className="player__song-info">Песня sdfsdf</p>
+          <span className="player__song-time">{songTime ? songTime : ''}</span>
+        </div>
+        <div className="player__range-box">
+          <div className="player__seeker">
+            <div
+              className="player__seeker-cover"
+              style={{ width: `${styleSeekerCover}%` }}></div>
+          </div>
+        </div>
+        <div className="player__songs-list"></div>
+      </div>
       <button type="button" className="player__control-btn"></button>
     </section>
   );
