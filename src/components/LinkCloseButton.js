bba3a547a8a815c7f0655d66555ce264ee28ab78
Yury Papkov <yu-papkoff@yandex.ru>
2020-11-04 17:53:17 +0300

    fixed: dropLinks animation and position

--- src/components/LinkCloseButton.js
+++ src/components/LinkCloseButton.js
@@ -2,8 +2,28 @@ import React from 'react';
 import closeIconPath from '../images/close-icon.svg'
 
 function LinkCloseButton({onClick}) {
+
+  const [pos, setPos]=React.useState(-60);
+  const [isVisible, setIsVisible] = React.useState(0);
+
+  React.useEffect(()=>{
+    setPos(0);
+    setIsVisible(1);
+    return()=>{
+      setPos(-60);
+      setIsVisible(0);
+    };
+  },[])
+
+  const shiftTrans = `translateY(${pos}px)`;
+  const cssRules = {
+    opacity: isVisible,
+    transform: shiftTrans,
+
+  };
+
   return (
-    <button className="linkCloseButton" onClick={onClick}>
+    <button style={cssRules} className="linkCloseButton" onClick={onClick}>
       <img src={closeIconPath} alt=""/>
     </button>
   );
