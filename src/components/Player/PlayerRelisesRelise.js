a0fb3b8e316d5c9c0bb16d4b508ef3d03bfe7734
Ivan Solnyshkin <ethosy@yandex.ru>
2020-11-06 02:54:24 +0700

    Updates

--- src/components/Player/PlayerRelisesRelise.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './PlayerRelisesRelise.css';
-
-// Одиночный релиз из массива релизов получает название
-const PlayerMenuLyric = ({ textSong }) => {
-  return <li className="player__relise-item">{textSong}</li>;
-};
-
-export default PlayerMenuLyric;
