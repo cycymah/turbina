96a30e71868f73a6191c536b9c92f4ba3adacc83
Yury Papkov <yu-papkoff@yandex.ru>
2020-11-07 19:38:58 +0300

    fixed: dropMenu review comments 1 step

--- src/components/LinkCloseButton.js
+++ src/components/LinkCloseButton.js
@@ -10,10 +10,6 @@ function LinkCloseButton({ onClick }) {
   React.useEffect(() => {
     setPos(0);
     setIsVisible(1);
-    return () => {
-      setPos(-60);
-      setIsVisible(0);
-    };
   }, [])
 
   const shiftTrans = `translateY(${pos}px)`;
@@ -24,7 +20,7 @@ function LinkCloseButton({ onClick }) {
   };
 
   return (
-    <button style={cssRules} className="linkCloseButton" onClick={onClick}>
+    <button style={cssRules} className="droplinks__close-button" onClick={onClick}>
       <svg width="12" height="12" viewBox="0 0 12 12" fill="none" stroke={color} xmlns="http://www.w3.org/2000/svg">
         <line x1="0.707107" y1="1.19231" x2="10.6066" y2="11.0918" strokeWidth="2" />
         <line x1="10.6075" y1="0.707107" x2="0.708003" y2="10.6066" strokeWidth="2" />
--- src/components/LinkHead.js
+++ src/components/LinkHead.js
@@ -3,12 +3,15 @@ import { COLOR as color } from '../constants/color';
 
 function LinkHead({ title, onClick }) {
   const [isVisible, setIsVisible] = React.useState(0);
-
+  //Здесь компонент монтируется невидимым, а потом включается, т.к. его задача -
+  //появиться плавно, по мере того, как кнопки уплывают вверх.
   React.useEffect(() => {
     setIsVisible(1);
   }, [])
   return (
-    <li className="linkHead" style={{ opacity: isVisible, borderColor: color, color: color }} onClick={onClick}>
+    <li className="droplinks__title"
+      style={{ opacity: isVisible, borderColor: color, color: color }}
+      onClick={onClick}>
       {title}
     </li>
   );
